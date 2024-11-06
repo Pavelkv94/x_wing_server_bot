9d9ec17c230ed7771a69233df5b4fa6b77c88732
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-11-07 01:29:18 +0300

    fix

--- src/modules/controls/shutdown.js
+++ src/modules/controls/shutdown.js
@@ -2,7 +2,7 @@ const shell = require("shelljs");
 const bot = require("../../bot/bot");
 
 module.exports = {
-  async shutdown() {
+  async shutdown(chatId) {
     await bot.sendMessage(chatId, "🟥 X-wing server will shutdown...");
 
     shell.exec(`echo ${process.env.SERVER_PASS} | sudo -S shutdown now`);
